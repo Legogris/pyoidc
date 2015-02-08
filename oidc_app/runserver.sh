01de0edb42e58541a3d12b3be9ebc5946d04d710
Robert Edstrom <legogris@legogris.se>
2015-02-08 18:19:31 +0900

    Runserver script update

--- oidc_app/runserver.sh
+++ oidc_app/runserver.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker run -itd -P --name openid_op --add-host="rp.legogris.se:127.0.0.1" legogris/openid_op
-docker run -itd -P --name openid_rp --add-host="idp.legogris.se:127.0.0.1" --net container:openid_op legogris/openid_rp
+docker run -itd -P --name openid_op --add-host="rp.legogris.se:127.0.0.1" --add-host="idp.legogris.se:127.0.0.1" legogris/openid_op
+docker run -itd -P --name openid_rp --net container:openid_op legogris/openid_rp
+exit
+docker run -it --rm -P --name openid_op --add-host="rp.legogris.se:127.0.0.1" --add-host="idp.legogris.se:127.0.0.1" legogris/openid_op
+docker run -it --rm -P --name openid_rp  --net container:openid_op legogris/openid_rp
